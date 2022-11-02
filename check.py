d3fd27778b4ff1e26700e42a9f094e78cbdde18b
Parth442002 <parthkatiyar04@gmail.com>
2022-11-02 23:25:20 +0530

    Working Prototype Created

--- check.py
+++ check.py
@@ -1,17 +1,13 @@
-import sys
-import tensorflow.keras
-import pandas as pd
-import sklearn as sk
-import scipy as sp
 import tensorflow as tf
-import platform
-print(f"Python Platform: {platform.platform()}")
-print(f"Tensor Flow Version: {tf.__version__}")
-print(f"Keras Version: {tensorflow.keras.__version__}")
-print()
-print(f"Python {sys.version}")
-print(f"Pandas {pd.__version__}")
-print(f"Scikit-Learn {sk.__version__}")
-print(f"SciPy {sp.__version__}")
-gpu = len(tf.config.list_physical_devices('GPU'))>0
-print("GPU is", "available" if gpu else "NOT AVAILABLE")
+
+cifar = tf.keras.datasets.cifar100
+(x_train, y_train), (x_test, y_test) = cifar.load_data()
+model = tf.keras.applications.ResNet50(
+    include_top=True,
+    weights=None,
+    input_shape=(32, 32, 3),
+    classes=100,)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
+model.fit(x_train, y_train, epochs=5, batch_size=64)
